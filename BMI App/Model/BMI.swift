ec5eb490eb4982343d406087619b74b6c5b84126
Andromeh <Andromeh@mail.ru>
2022-03-04 10:58:40 +0300

    Complete App

---
+++ BMI App/Model/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Белозеров Константин on 17.01.2022.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+    
+}
